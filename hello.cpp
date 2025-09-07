c9215ddda306a8f3a74519a9ec98aa7ee1bdf551
vietlehuy2024 <vietlehuy2024@gmail.com>
2025-09-07 16:11:22 +0700

    Add project files.

---
+++ hello.cpp
@@ -0,0 +1,44 @@
+﻿// hello.cpp : Defines the entry point for the application.
+//
+
+#include "hello.h"
+
+using namespace std;
+
+const int WIDTH = 40;
+const int HEIGHT = 20;
+
+void clearScreen() {
+    cout << "\033[2J\033[1;1H"; // ANSI escape code to clear screen
+}
+
+void drawFrame(const vector<string>& buffer) {
+    clearScreen();
+    for (const auto& line : buffer) {
+        cout << line << endl;
+    }
+}
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+    vector<string> buffer(HEIGHT, string(WIDTH, ' '));
+
+    // Draw a box
+    for (int x = 0; x < WIDTH; ++x) {
+        buffer[0][x] = '-';
+        buffer[HEIGHT - 1][x] = '-';
+    }
+    for (int y = 0; y < HEIGHT; ++y) {
+        buffer[y][0] = '|';
+        buffer[y][WIDTH - 1] = '|';
+    }
+
+    buffer[0][0] = buffer[0][WIDTH - 1] = '+';
+    buffer[HEIGHT - 1][0] = buffer[HEIGHT - 1][WIDTH - 1] = '+';
+
+    drawFrame(buffer);
+
+    this_thread::sleep_for(chrono::seconds(3));
+	return 0;
+}
---
+++ hello.h
@@ -0,0 +1,11 @@
+﻿// hello.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+
+// TODO: Reference additional headers your program requires here.
